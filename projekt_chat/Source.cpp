d7397125b342c90d3ba903ab430561464d0ec386
Mathias Allesen Lyngbye <malyn16@student.sdu.dk>
2017-11-15 15:04:55 +0100

    Fixed recording and interpreting. This commit saved my marriage.

--- projekt_chat/Source.cpp
+++ projekt_chat/Source.cpp
@@ -5,16 +5,19 @@
 
 int main()
 {
-	/*
-	DLL myDLL("Hello india");
-	myDLL.send("Hello india");
-	int var;
-	*/ 
 
-	DTMF dtmf(500);
-	dtmf.startRecording();
-	
-	//dtmf.play_wait(DTMF_1);
+
+	DLL myDLL("Hello india");
+	myDLL.read(1000);
+
+
+//	cout << myDLL.interpret(DTMF_A) << endl;
+
+//	myDLL.send("Hello india");
+
+//	dtmf.play_wait(DTMF_1);
+
+
 
 	int x;
 	cin >> x;
